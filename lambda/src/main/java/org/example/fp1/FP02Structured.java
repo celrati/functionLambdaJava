a562122c464d27b6fa8510010c0a7fb5531d9c97
celrati <m.charif.achraf@gmail.com>
2023-03-02 00:12:38 +0100

    update

--- lambda/src/main/java/org/example/fp1/FP02Structured.java
+++ lambda/src/main/java/org/example/fp1/FP02Structured.java
@@ -11,13 +11,50 @@ public class FP02Structured {
       int sum2 = addListFunctional(numbers);
       System.out.println(sum);
       System.out.println(sum2);
+
+      exercices();
+      distinctExercice();
+      sortingExercice();
+    }
+
+    public static void sortingExercice() {
+        // we can so it for strings also..
+        List<Integer> numbers = List.of(0, 1,2,3,4,5,6, 0, 4 ,5,6);
+        numbers.stream()
+                .sorted()
+                .forEach(System.out::print);
+        // by default the comparator is the Comparator.naturalOrder()
+        // we have also the Comparator.reverseOrder()
+        // custom comparator
+        // .sorted(Comparator.comparing(str -> str.length())).forEach(showing here)
+    }
+    public static void distinctExercice() {
+        List<Integer> numbers = List.of(0, 1,2,3,4,5,6, 0, 4 ,5,6);
+        numbers.stream()
+                .distinct()
+                .forEach(System.out::print);
+    }
+
+    public static void exercices() {
+        List<Integer> numbers = List.of(0, 1,2,3,4,5,6);
+        int s = numbers.stream()
+                .map(number -> number * number)
+              //  .reduce(0, (x, y) -> x + y);
+                .reduce(0, Integer::sum);
+
+        int s2 = numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .reduce(0, Integer::sum);
+
+        System.out.println(s);
+        System.out.println(s2);
     }
 
     private static int sum(int aggregate, int nextNumber) {
         return aggregate + nextNumber;
     }
     private static int addListFunctional(List<Integer> numbers) {
-        // needs the accumalot and the combiner "sum"
+        // needs the accumalator and the combiner "sum"
         return numbers.stream()
                 //.reduce(0, FP02Structured::sum);
                 .reduce(0, (x,y) -> x + y);
